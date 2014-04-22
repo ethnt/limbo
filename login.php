fd789f8a73fb3d2eed16329e757b6f939e9eb854
mmaffa <matthewmaffa@gmail.com>
2014-04-22 18:07:17 -0400

    removed code for other page

--- login.php
+++ login.php
@@ -5,16 +5,8 @@
 <?php require('includes/partials/header.php'); ?>
 
 <?php
-  $items = run('select * from users where email=;');
+	
 
-  foreach ($item in $items){
-
-  echo "<div id= 'item'>
-        <p id = 'item_name'> " . $item['name'] ."</p><p id='description'>". 
-        $item['description'] . " </p><p id='time_location'>" .
-        "found on " . $item['timestamp'] . "at" . $item['location'];
-		" </div>";
-    }
 
 ?>
 
