4b6ac3bbf881fdb48dda52fbd94a3f52829ddd64
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-04-22 12:47:26 -0400

    added gulp-include and all task.

--- README.md
+++ README.md
@@ -5,3 +5,25 @@
 ---
 
 Limbo is a project for CMPT221L that keeps track of lost and found items.
+
+## Development
+
+Limbo uses [gulp.js](http://gulpjs.com/) and [Bower](http://bower.io) to manage assets. To install the depedencies, you need [Node.js](http://nodejs.org) and [npm](https://www.npmjs.org) installed. Then, run:
+
+```
+$ npm install
+```
+
+To get the assets from bower, run:
+
+```
+$ gulp bower
+```
+
+Finally, to compile our assets (SCSS and CoffeeScript), you can run this to compile all of them:
+
+```
+$ gulp all
+```
+
+See `gulpfile.js` for more options.
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-var gulp    = require('gulp');
-var watch   = require('gulp-watch');
-var bower   = require('gulp-bower');
-var sass    = require('gulp-sass');
-var coffee  = require('gulp-coffee');
+var gulp    = require('gulp'),
+    watch   = require('gulp-watch'),
+    bower   = require('gulp-bower'),
+    include = require('gulp-include'),
+    sass    = require('gulp-sass'),
+    coffee  = require('gulp-coffee');
 
 gulp.task('default', function() {
   gulp.src('./assets/src/css/*.scss')
@@ -22,6 +23,17 @@ gulp.task('bower', function() {
   bower()
 });
 
+gulp.task('all', function() {
+  gulp.src('./assets/src/css/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('./assets/dist/css'));
+
+  gulp.src('./assets/src/jsc/*.coffee')
+      .pipe(include())
+      .pipe(coffee({ bare: true }))
+      .pipe(gulp.dest('./assets/dist/jsc'));
+});
+
 gulp.task('sass', function() {
   gulp.src('./assets/src/css/*.scss')
       .pipe(sass())
@@ -30,6 +42,7 @@ gulp.task('sass', function() {
 
 gulp.task('coffee', function() {
   gulp.src('./assets/src/jsc/*.coffee')
+      .pipe(include())
       .pipe(coffee({ bare: true }))
       .pipe(gulp.dest('./assets/dist/jsc'));
 });
